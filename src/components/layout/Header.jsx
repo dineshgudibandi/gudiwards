5328c06132f819e13f7fc5e518ddae9fafac92ef
Dinesh Gudibandi <dineshreddyy@gmail.com>
2026-01-19 20:33:02 -0600

    added logo

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,7 +3,7 @@ import {ToggleButton} from "../shared/ToggleButton";
 export function Header({ role, setRole, kids }) {
     return (
         <header className="flex justify-between items-center">
-            <h1 className="text-3xl font-extrabold text-indigo-700"><img src="/img/logo.png" alt="Gudiwards" className="mx-auto w-24 h-24 rounded-full shadow-lg" /></h1>
+            <h1 className="text-3xl font-extrabold text-indigo-700"><img src="https://dineshgudibandi.github.io/gudiwards/img/logo.png" alt="Gudiwards" className="mx-auto w-24 h-24 rounded-full shadow-lg" /></h1>
             <div className="flex bg-white rounded-full shadow overflow-hidden">
                 <ToggleButton active={role === "user"} onClick={() => setRole("user")} label="Home" />
                 <ToggleButton active={role === "parent"} onClick={() => setRole("parent")} label="Parent" />
